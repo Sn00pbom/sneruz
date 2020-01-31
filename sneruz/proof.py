a080d9818fcc2f9e1ae69a100ae4897e4ef05d19
Zach Lefin <zachlefin@gmail.com>
2020-01-31 12:04:57 -0700

    changed variables

--- sneruz/proof.py
+++ sneruz/proof.py
@@ -4,28 +4,25 @@ class Proof(object):
 
     def __init__(self):
         super().__init__()
-        self._steps = []
-        self._premises = []
+        self.steps = []
+        self.premises = []
 
     def __len__(self):
-        return len(self._steps)
+        return len(self.steps)
+
+    def __getitem__(self, key):
+        return self.steps[key-1]
 
     def suppose(self, prop):
         """Add a proposition as a step and a premise"""
-        self._steps.append(prop)
-        self._premises.append(prop)
+        self.steps.append(prop)
+        self.premises.append(prop)
     
     def therefore(self, prop):
         """Add a proposition as a step"""
-        self._steps.append(prop)
+        self.steps.append(prop)
 
-    def get_step(self, prop_i) -> "Prop":
-        """Return a step in the sequence from [1,len]"""
-        return self._steps[i-1]
-    
-    def get_steps(self) -> list:
-        return self._steps
-
+    @property
     def last(self) -> "Prop":
         """Returns the last proposition added"""
-        return self._steps[-1]
+        return self.steps[-1]
