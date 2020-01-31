1c91d83fdc73e8469b4ffaa47ddb9b22ca8f8bf7
Zach Lefin <zachlefin@gmail.com>
2020-01-31 12:14:51 -0700

    changed premises to conditions

--- sneruz/proof.py
+++ sneruz/proof.py
@@ -5,7 +5,7 @@ class Proof(object):
     def __init__(self):
         super().__init__()
         self.steps = []
-        self.premises = []
+        self.conditions = []
 
     def __len__(self):
         return len(self.steps)
@@ -13,10 +13,10 @@ class Proof(object):
     def __getitem__(self, key):
         return self.steps[key-1]
 
-    def suppose(self, prop):
-        """Add a proposition as a step and a premise"""
+    def condition(self, prop):
+        """Add a proposition as a step and a condition"""
         self.steps.append(prop)
-        self.premises.append(prop)
+        self.conditions.append(prop)
     
     def therefore(self, prop):
         """Add a proposition as a step"""
