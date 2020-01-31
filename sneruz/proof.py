2d19b6e24c7ff1043ca66d65bab2efa5fbaad76e
Zach Lefin <zachlefin@gmail.com>
2020-01-31 11:52:02 -0700

    changed adds to suppose and therefore

--- sneruz/proof.py
+++ sneruz/proof.py
@@ -2,19 +2,29 @@
 
 class Proof(object):
 
-    def __init__(self, steps: list=None):
+    def __init__(self):
         super().__init__()
-        self._steps =  steps if steps else []
+        self._steps = []
+        self._premises = []
 
     def __len__(self):
         return len(self._steps)
+
+    def suppose(self, prop):
+        """Add a proposition as a step and a premise"""
+        self._steps.append(prop)
+        self._premises.append(prop)
     
-    def add_step(self, prop):
+    def therefore(self, prop):
+        """Add a proposition as a step"""
         self._steps.append(prop)
 
     def get_step(self, prop_i) -> "Prop":
         """Return a step in the sequence from [1,len]"""
         return self._steps[i-1]
+    
+    def get_steps(self) -> list:
+        return self._steps
 
     def last(self) -> "Prop":
         """Returns the last proposition added"""
