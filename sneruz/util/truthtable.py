60db1afe645032396a0e09b9e4242556596bbaa7
Zach Lefin <zachlefin@gmail.com>
2020-02-06 00:23:32 -0700

    fixed some stuff. working md

--- sneruz/util/truthtable.py
+++ sneruz/util/truthtable.py
@@ -5,29 +5,38 @@ from sneruz.proof import Proof
 
 class TruthTable(object):
 
-    def __init__(self, proof: Proof=None):
+    def __init__(self, proof: Proof):
         super().__init__()
-        self._steps = []
+        self._columns = []
         self._rows = []
+        self._proof = proof
 
-    def set_column_names(names: list):
-        self._columns = names
-    
-    def add_column(self, prop):
-        self._steps.add(prop)
+    def generate(self, column_names=None):
+        self._columns = column_names
+        if column_names is None:
+            self._columns = [" " for _ in len(self._proof)]
+        
+        for v in product([True, False], repeat=len(self._proof.premises)):
+            s = []
+            for i in range(len(v)):
+                self._proof.premises[i].set(v[i])
 
-    def set_steps(self, props):
-        """Sets steps to props if props is list. If props is Proof, imports steps"""
-        if isinstance(props, list):
-            self._steps = props
-        elif isinstance(props, Proof):
-            self._steps = props.get_steps()
+            for p in self._proof:
+                s.append(p())
 
-    def generate(self):
-        pass
+            self._rows.append(s)
 
     def export_csv(self):
         pass
 
     def export_md(self):
+        s = [c for c in self._columns]
+        print('|'.join(s))
+        s = [':---:' for _ in self._columns]
+        print('|'.join(s))
+        for row in self._rows:
+            s = ['T' if r else 'F' for r in row]
+            print('|'.join(s))
+
+    def export_html(self):
         pass
